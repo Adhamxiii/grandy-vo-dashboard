1a215b74c0104e859c7afeee2cd6e2d79d334a0c
Adham Nasser <150140392+Adhamxiii@users.noreply.github.com>
2024-11-26 23:02:10 +0200

    add post contact

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,11 +3,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-    const contacts = await prisma.contact.findMany();
-    return new Response(JSON.stringify(contacts), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+  const contacts = await prisma.contact.findMany();
+  return new Response(JSON.stringify(contacts), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  const { name, email, message } = await request.json();
+
+  const newMessage = await prisma.contact.create({
+    data: {
+      name,
+      email,
+      message,
+    },
+  });
+
+  return new Response(JSON.stringify(newMessage), {
+    status: 201,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
