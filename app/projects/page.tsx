1259a4a53cbe1732e42e57f9adaf369481b6c0d9
Adham Nasser <150140392+Adhamxiii@users.noreply.github.com>
2024-11-26 21:46:35 +0200

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,17 +1,15 @@
 import Projects from "@/components/Projects";
-
+import axios from "axios";
 
 const getProjects = async () => {
-  const res = await fetch(`http://localhost:3000/api/projects`, {
-    cache: "no-store",
-  });
-  if (!res.ok) {
+  const res = await axios.get("https://grandy-vo-dashboard.vercel.app/api/projects");
+  if (!res) {
     throw new Error("Failed to fetch projects");
   }
-  return res.json();
+  return res.data;
 };
 export default async function page() {
   const projects = await getProjects();
-  
+
   return <Projects initialProjects={projects} />;
 }
