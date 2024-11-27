bc29dba0afbf8bfa1a748a02cfe621083f1e2099
Adham Nasser <150140392+Adhamxiii@users.noreply.github.com>
2024-11-27 21:11:17 +0200

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,11 @@
 import Projects from "@/components/Projects";
 import axios from "axios";
 
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
 const getProjects = async () => {
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/projects`
-  );
+  const res = await axios.get(`/api/projects`);
   if (!res) {
     throw new Error("Failed to fetch projects");
   }
@@ -12,6 +13,8 @@ const getProjects = async () => {
 };
 export default async function page() {
   const projects = await getProjects();
+
+  console.log(projects);
 
   return <Projects initialProjects={projects} />;
 }
